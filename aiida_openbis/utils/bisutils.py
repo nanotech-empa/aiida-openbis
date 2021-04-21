22c3b1975f82dd8a0e02ab76dc8816ccfa585744
Carlo Pignedoli <c.pignedoli@gmail.com>
2021-04-21 17:11:04 +0200

    bug fix

--- aiida_openbis/utils/bisutils.py
+++ aiida_openbis/utils/bisutils.py
@@ -145,7 +145,7 @@ def new_reaction_products(reactions=None, molecules=None, attachment=None):
     for reac in reactions:
         print('adding ', reac['product'], ' id ', allobj[reac['product']].permId)
         print('as child of ', reac['reactant'], ' id ', allobj[reac['reactant']].permId)
-        allobj[reac['reactant']].add_children(allobj[reac['product']])
+        allobj[reac['reactant']].add_children(allobj[reac['product']].permId)
         allobj[reac['reactant']].save()
         if reac['yield']:
             allobj[reac['product']].props['molproduct.yield'] = reac['yield']
