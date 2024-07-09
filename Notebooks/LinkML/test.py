7b74545a9e54ec115e31052543ea3da4ea2d4689
Fabio Lopes <fabioacl96@gmail.com>
2024-07-09 16:27:11 +0200

    Remove not used folders

--- Notebooks/LinkML/test.py
+++
@@ -1,5 +0,0 @@
-from personinfo import Person, PersonStatus
-
-person = Person(id='P1', full_name='Julius Caesar', status="DEAD")
-print(f'STATUS={person.status}')
-print(f'MEANING={person.status.meaning}')
